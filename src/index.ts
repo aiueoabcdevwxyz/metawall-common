856d5d659c789f208b8312a6ef92fb19edd783b7
aiueoabcdevwxyz <financial.fantasy@gmail.com>
2020-11-14 21:50:06 +0900

    vdfvdfvdf

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,6 @@ export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
 export * from './events/order-cancelled-event';
 export * from './events/user-created-event';
+export * from './events/post-created-event';
 export * from './events/expiration-complete-event';
 export * from './events/payment-created-event';
